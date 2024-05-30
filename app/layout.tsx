ee5d75d5fc6211ac7c5b34416254b140f861916e
mhbagheri-99 <mhbagheri3@gmail.com>
2024-05-30 19:51:36 +0200

    Init Project

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({ subsets: ["latin"], weight: ['400', '500', '600', '700'], variable: '--font-poppins', });
+
+export const metadata: Metadata = {
+  title: "Evently",
+  description: "Your event management companion",
+  icons: {
+    icon: 'favicon.ico',
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
+}
